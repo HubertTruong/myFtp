9eff2a36f1a0d6b951154ebd7ff6edfd31ff80a3
Hubert Truong <il03548@etu.parisdescartes.fr>
2021-12-07 16:53:51 +0100

    2nd commit - new code
    Code edition, removed switch case

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,7 @@ export function launch(port) {
       const [command, ...args] = message.trim().split(" ");
       console.log(command, args);
 
-      switch(command) {
+      /*switch(command) {
         case "USER":
           socket.write("230 User logged in, proceed.\r\n");
           break;
@@ -25,10 +25,34 @@ export function launch(port) {
         case "TYPE":
           socket.write("200 \r\n");
           break;
+        case "EPSV":
+          socket.write("229 \r\n");
+          break;
+        case "EPRT":
+          socket.write("200 \r\n");
+          break;
         default:
           console.log("command not supported:", command, args);
       }
-    });
+    }*/
+    
+    const directives = {
+      USER: (socket) => { socket.write("230 User logged in, proceed. \r\n") },
+      PASS:(socket) => { socket.write(" \r\n") },
+      LIST:(socket) => { socket.write(" \r\n") },
+      CWD: (socket) => { socket.write(" \r\n") },
+      RETR: (socket) => { socket.write(" \r\n") },
+      STOR: (socket) => { socket.write(" \r\n") },
+      PWD: (socket) => { socket.write("257 /users/hubert\r\n") },
+      HELP: (socket) => { socket.write("214 Help message. \r\n") },
+      QUIT: (socket) => { socket.write("221 Service closing control connection. \r\n") }
+    }
+    
+    if(directives[command] === undefined) {
+      socket.write("5XX unsupported command \r\n");
+    }
+
+  });
 
     socket.write("220 Hello World \r\n");
   });
