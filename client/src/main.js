610f7f60912a7e88fe93aae431b3615603446c55
HubertTruong <hubert.truong01@gmail.com>
2021-12-08 16:32:36 +0100

    4th commit - reoganization
    Reorganization of the code, added back the switch case and start of coding the commands

--- client/src/main.js
+++ client/src/main.js
@@ -1,28 +1,31 @@
 import { createConnection } from "net";
+import { createInterface } from "readline";
 
 let currentCommand = '';
 let isAuthenticated = false;
 
+const rl = createInterface({
+  input: process.stdin, 
+  output: process.stdout
+});
+
 const client = createConnection({ port: 4242 }, () => {
   console.log("client connected.");
+  rl.question("Enter a command please: ", function (cmd) {
+    client.write(cmd)
+  })
 });
 
 client.on("data", (data) => {
   const message = data.toString();
   console.log("Message received:", message);
+  rl.question("Enter a command please: ", function(cmd){
+    client.write(cmd)
+  })
 
   const [status, ...args] = message.trim().split(" ");
   if (status == 230 && currentCommand === "USER") {
     isAuthenticated = true;
   }
 
-  if (status == 220) {
-    currentCommand = "USER";
-    client.write("USER anonymous");
-  };
-
-  if (status == 221) {
-    currentCommand = "QUIT";
-    client.write("Service closing control connection.");
-  };
 });
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import { createServer } from "net";
 
 export function launch(port) {
+
   const server = createServer((socket) => {
     console.log("new connection.");
     socket.on("data", (data) => {
@@ -8,40 +9,37 @@ export function launch(port) {
 
       const [command, ...args] = message.trim().split(" ");
       console.log(command, args);
-    
-      const directives = {
-        USER: (socket) => { 
-          socket.write("230 User logged in, proceed. \r\n") 
-        },
-        PASS:(socket) => { 
-          socket.write("230 User logged in, proceed. \r\n") 
-        },
-        LIST:(socket) => { 
-          socket.write("125 Data connection already open; transfer starting. \r\n") 
-        },
-        CWD: (socket) => { 
-          socket.write("250 Requested file action okay, completed. \r\n") 
-        },
-        RETR: (socket) => { 
-          socket.write(" \r\n") 
-        },
-        STOR: (socket) => { 
-          socket.write(" \r\n") 
-        },
-        PWD: (socket) => { 
-          socket.write("257 /users/hubert \r\n") 
-        },
-        HELP: (socket) => { 
-          socket.write("214 Help message. \r\n") 
-        },
-        QUIT: (socket) => { 
-          socket.write("221 Service closing control connection. \r\n")
-          socket.end(); 
-        }
-      }
-    
-      if(directives[command] === undefined) {
-        socket.write("5XX unsupported command \r\n");
+
+      switch(command) {
+        case "USER": 
+          socket.write("230 User logged in, proceed. \r\n");
+          break;
+        case "PASS": 
+          socket.write("331 User name ok, need pass. \r\n");
+          break;
+        case "LIST": 
+          socket.write("125 Data connection already open; transfer starting. \r\n");
+          break;
+        case "CWD": 
+          socket.write(`250 Requested file action okay, completed. ${process.chdir()} \r\n`);
+          break;
+        case "RETR": 
+          socket.write("125 \r\n");
+          break;
+        case "STOR": 
+          socket.write("125 \r\n");
+          break;
+        case "PWD": 
+          socket.write(`257 Requested file action okay, completed. \r\n ${process.cwd()} \r\n`);
+          break;
+        case "HELP": 
+          socket.write("214 Help message. \r\n");
+          break;
+        case "QUIT": 
+          socket.write(`221 Service closing control connection. \r\n ${process.exit()} \r\n`);
+          break;
+        default:
+          console.log("Command not supported");
       }
 
     });
