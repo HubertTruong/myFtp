e091d6118f1e6811d0335255b817cf8edfb5fca7
HubertTruong <hubert.truong01@gmail.com>
2021-12-08 21:14:07 +0100

    7th commit - LIST command done
    Finalization of the LIST command, starting the identation and cleaning of the code

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,6 @@
 import { createServer } from "net";
-import { fs } from "fs";
+
+const fs = require("fs");
 
 export function launch(port) {
 
@@ -15,48 +16,59 @@ export function launch(port) {
         case "USER": 
           socket.write("230 User logged in, proceed. \r\n");
           break;
+
         case "PASS": 
           socket.write("331 User name ok, need pass. \r\n");
           break;
+
         case "LIST":
           socket.write("125 Data connection already open; transfer starting. \r\n");
-          /*let files = fs.readdirSync(__dirname);
+          let files = fs.readdirSync(process.cwd());
+          let answer = "";
+          answer += "\r\n-- Files in the current directory --\r\n"
           files.forEach(file => {
-            console.log(file);
-          });*/
+            answer += `${file}\r\n`;
+          });
+          socket.write(answer);
           break;
+
         case "CWD": 
           socket.write(`250 Requested file action okay, completed. ${process.chdir()} \r\n`);
           break;
+          
         case "RETR": 
           socket.write("125 \r\n");
           break;
+
         case "STOR": 
           socket.write("125 \r\n");
           break;
+
         case "PWD": 
           socket.write(`257 Requested file action okay, completed. \r\n ${process.cwd()} \r\n`);
           break;
+
         case "HELP":   
           socket.write(`211 System status, or system help reply. \r\n`);
-          const fs = require('fs');
           fs.readFile(`${__dirname}/commands.json`, 'utf8', (err, data) => {
               if (err) {
                   console.log(`Error reading file from disk: ${err}`);
               } else {
-                  let resp = ""; 
-                  resp += "\r\n-- Commands --\r\n"
+                  let response = ""; 
+                  response += "\r\n-- Commands --\r\n"
                   const commands = JSON.parse(data);
                   commands.forEach(cm => {
-                      resp += `•${cm.command}: ${cm.desc}\r\n`
+                    response += `•${cm.command}: ${cm.desc}\r\n`;
                   });
-                  socket.write(resp);
+                  socket.write(response);
               }
           });
           break;
+
         case "QUIT": 
           socket.write(`221 Service closing control connection. \r\n ${process.exit()} \r\n`);
           break;
+          
         default:
           console.log("Command not supported");
       }
