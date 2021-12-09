f9fc477eb1a88929c0b16af6b8553afe033b354a
Hubert Truong <il03548@etu.parisdescartes.fr>
2021-12-09 11:56:04 +0100

    12th commit - code reorganization
    Reorganization of the code

--- server/src/server.js
+++ server/src/server.js
@@ -8,38 +8,41 @@ export function launch(port) {
     console.log("new connection.");
     socket.on("data", (data) => {
       const message = data.toString();
-
       const [command, ...args] = message.trim().split(" ");
       console.log(command, args);
 
       switch(command) {
+
         case "USER": 
           fs.readFile(`${__dirname}/user.json`, 'utf8', (err, data) => {
             if (err) {
-                console.log(`Error reading file from disk: ${err}`);
-            } else {
-                socket.user = args[0];
-                let response = ""; 
-                const commands = JSON.parse(data);
-                commands.forEach(cm => {
-                  if(socket.user == cm.user){
-                    socket.pass = cm.pass
-                    response += "\n230 Username recognized, use the PASS command to enter your password now\n";
-                  }
-                  else
-                    response += "\n530 Not logged in: Username unrecognized, please try again\n";
-                });
-                socket.write(response);
+              console.log(`Error reading file from disk: ${err}`);
+            } 
+            else {
+              socket.user = args[0];
+              let response = ""; 
+              const commands = JSON.parse(data);
+              commands.forEach(cm => {
+                if(socket.user == cm.user) {
+                  socket.pass = cm.pass
+                  response += "\n230 Username recognized: use the PASS command to enter your password now\n";
+                }
+                else
+                  response += "\n530 Not logged in: Username unrecognized, please try again\n";
+              });
+              socket.write(response);
             }
           });
           break;
 
         case "PASS": 
-          if(args[0] != socket.pass){
+          if(args[0] != socket.pass) {
             socket.write("331 Username recognized but wrong password, please retry. \r\n");
-          } else if(args[0] == socket.pass){
+          } 
+          else if(args[0] == socket.pass) {
             socket.write("230 Username and Password valid, logging in \r\n");
-          } else if(args[0] == undefined) {
+          } 
+          else if(args[0] == undefined) {
             socket.write("501 Syntax error in parameters or arguments. \r\n");
           }
           break;
@@ -81,14 +84,15 @@ export function launch(port) {
           fs.readFile(`${__dirname}/commands.json`, 'utf8', (err, data) => {
               if (err) {
                   console.log(`Error reading file from disk: ${err}`);
-              } else {
-                  let response = ""; 
-                  response += "\r\n-- Commands --\r\n"
-                  const commands = JSON.parse(data);
-                  commands.forEach(cm => {
-                    response += `•${cm.command}: ${cm.desc}\r\n`;
-                  });
-                  socket.write(response);
+              } 
+              else {
+                let response = ""; 
+                response += "\r\n-- Commands --\r\n"
+                const commands = JSON.parse(data);
+                commands.forEach(cm => {
+                  response += `•${cm.command}: ${cm.desc}\r\n`;
+                });
+                socket.write(response);
               }
           });
           break;
@@ -100,7 +104,6 @@ export function launch(port) {
         default:
           console.log("500 Command not supported");
       }
-
     });
     socket.write("220 Hello World \r\n");
   });
